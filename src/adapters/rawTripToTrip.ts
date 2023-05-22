55a6e4e03c473ac60d7a56a4eb9e0ec10252726a
Stephano Ramos <stephano.ramos@uber.com>
2023-05-22 00:40:38 -0300

    refactors

--- src/adapters/rawTripToTrip.ts
+++ src/adapters/rawTripToTrip.ts
@@ -3,6 +3,7 @@ import { rawStudentToStudent } from "./rawStudentToStudent";
 import { parseISO } from "date-fns";
 import { rawItineraryToItinerary } from "./rawItineraryToItinerary";
 import { rawStudentTripPivotToStudentTripPivot } from "./rawStudentTripPivotToStudentTripPivot";
+import { utcToZonedTime } from "date-fns-tz";
 
 export const rawTripToTrip = async (rawTrip: RawTrip): Promise<Trip> => ({
   id: rawTrip.id,
@@ -11,11 +12,17 @@ export const rawTripToTrip = async (rawTrip: RawTrip): Promise<Trip> => ({
   arriveAt: parseISO(rawTrip.arrive_at),
   latitude: rawTrip.latitude ?? undefined,
   longitude: rawTrip.longitude ?? undefined,
-  startedAt: rawTrip.started_at ? parseISO(rawTrip.started_at) : undefined,
-  finishedAt: rawTrip.finished_at ? parseISO(rawTrip.finished_at) : undefined,
+  startedAt: rawTrip.started_at
+    ? utcToZonedTime(parseISO(rawTrip.started_at), "America/Sao_Paulo")
+    : undefined,
+  finishedAt: rawTrip.finished_at
+    ? utcToZonedTime(parseISO(rawTrip.finished_at), "America/Sao_Paulo")
+    : undefined,
   itinerary: await rawItineraryToItinerary(rawTrip.itinerary),
-  createdAt: parseISO(rawTrip.created_at),
-  updatedAt: rawTrip.updated_at ? parseISO(rawTrip.updated_at) : undefined,
+  createdAt: utcToZonedTime(parseISO(rawTrip.created_at), "America/Sao_Paulo"),
+  updatedAt: rawTrip.updated_at
+    ? utcToZonedTime(parseISO(rawTrip.updated_at), "America/Sao_Paulo")
+    : undefined,
   students: Array.isArray(rawTrip.students)
     ? await Promise.all(rawTrip.students.map(rawStudentToStudent))
     : undefined,
